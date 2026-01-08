a3d3ad7cf48bad05d49f1a2b25fe08d6c9999052
もちこ♥ <mochitarouji@gmail.com>
2026-01-09 05:27:58 +0900

    i loved arrow functions but now i avoid them

---
+++ README.md
@@ -0,0 +1,20 @@
+#Installation
+npm i mochiko666/managers
+#Environments
+node v25.2.0
+npm 11.6.2
+typescript@5.9.3
+just see package.json/tsconfig
+#Example
+```ts
+import path from "path";
+import { RecordFileManager, MapFileManager } from "managers";
+import { User } from "../types/user.js";
+const usersPath = path.join(process.cwd(), "cache", "users.json");
+const manager = await MapFileManager.init<User>(usersPath);
+await manager.add("user666", { id: 666, name: "user666" });
+
+// if you want to initialize manager with empty cache, just use constructor
+const emptyCache = new RecordFileManager.init<User>(usersPath);
+console.log(emptyCache.cache); // {}
+```
--- src/classes/manager.ts
+++ src/classes/manager.ts
@@ -26,19 +26,54 @@ export abstract class FileCacheManager<Cache> extends BaseCacheManager<Cache> {
   }
 }
 
-export abstract class IOCacheManager<Cache> extends FileCacheManager<Cache> {
+export abstract class JsonCacheManager<Cache> extends FileCacheManager<Cache> {
   protected readonly queue: PromiseQueue = new PromiseQueue();
-  public abstract load(): Promise<Cache>;
-  public abstract loadSync(): Cache;
+  public async load(): Promise<
+    Cache extends Array<infer Holds>
+      ? ReadonlyArray<Holds>
+      : Cache extends Map<infer K, infer V>
+      ? ReadonlyMap<K, V>
+      : Readonly<Cache>
+  > {
+    return this.queue.set(async () => {
+      try {
+        const raw = await fs.readFile(this.path, "utf-8");
+        return (this.__cache = JSON.parse(raw));
+      } catch (e: unknown) {
+        if (isFSError(e) && e.code === "ENOENT") return this.cache;
+        throw e;
+      }
+    });
+  }
+  public loadSync(): Cache extends Array<infer Holds>
+    ? ReadonlyArray<Holds>
+    : Cache extends Map<infer K, infer V>
+    ? ReadonlyMap<K, V>
+    : Readonly<Cache> {
+    try {
+      const raw = readFileSync(this.path, "utf-8");
+      return (this.__cache = JSON.parse(raw));
+    } catch (e: unknown) {
+      if (isFSError(e) && e.code === "ENOENT") return this.cache;
+      throw e;
+    }
+  }
+  public async save(): Promise<void> {
+    return this.queue.set(async () => {
+      const tmpPath = this.path + ".tmp";
+      await fs.writeFile(tmpPath, JSON.stringify(this.cache, null, 2));
+      await fs.rename(tmpPath, this.path);
+    });
+  }
 }
 
-export class ListFileManager extends IOCacheManager<string[]> {
+export class ListFileManager<Cache> extends JsonCacheManager<Cache[]> {
   /**
    * Creates an instance of ListManager by loading from file asynchronously.
    * @param path The file path to store the list.
    */
-  public static async init(path: PathLike): Promise<ListFileManager> {
-    const instance = new ListFileManager(path);
+  public static async init<Cache>(path: PathLike): Promise<ListFileManager<Cache>> {
+    const instance = new ListFileManager<Cache>(path);
     await instance.load();
     return instance;
   }
@@ -46,69 +81,47 @@ export class ListFileManager extends IOCacheManager<string[]> {
    * Creates an instance of ListManager by loading from file synchronously.
    * @param path The file path to store the list.
    */
-  public static initSync(path: PathLike): ListFileManager {
-    const raw = readFileSync(path, "utf-8"); // 123456789\n987654321
-    return new ListFileManager(path, raw.split("\n")); // ["123456789", "987654321"]
+  public static initSync<Cache>(path: PathLike): ListFileManager<Cache> {
+    const instance = new ListFileManager<Cache>(path); // ["123456789", "987654321"]
+    instance.loadSync();
+    return instance;
   }
   /**
-   * Creates an instance of ListManager. Use `ListManager.init()` or `ListManager.initSync()`
-   * to initialize by loading from file.
+   * Creates an instance of ListManager. \
+   * Use `ListManager.init()` or `ListManager.initSync()` to initialize by loading from file.
    * @param path The file path to store the list.
    * @param init The initial array list.
    */
-  public constructor(path: PathLike, init: string[] = []) {
+  public constructor(path: PathLike, init: Cache[] = []) {
     super(path, init);
   }
-  public load = async (): Promise<string[]> =>
-    this.queue.set(async () => {
-      try {
-        const raw = await fs.readFile(this.path, "utf-8"); // 123456789\n987654321
-        return (this.__cache = raw
-          .split("\n")
-          .map((s) => s.trim())
-          .filter(Boolean)); // ["123456789", "987654321"]
-      } catch (e: unknown) {
-        if (isFSError(e) && e.code === "ENOENT") return [];
-        throw e;
-      }
-    });
-  public loadSync(): string[] {
-    try {
-      const raw = readFileSync(this.path, "utf-8"); // 123456789\n987654321
-      return (this.__cache = raw
-        .split("\n")
-        .map((s) => s.trim())
-        .filter(Boolean)); // ["123456789", "987654321"]
-    } catch (e: unknown) {
-      if (isFSError(e) && e.code === "ENOENT") return [];
-      throw e;
-    }
+
+  public has(item: Cache): boolean {
+    return this.cache.includes(item);
   }
-  public save = async (): Promise<void> =>
-    this.queue.set(async () => {
-      const uniq = Array.from(new Set(this.cache.map((s) => s.trim()).filter(Boolean)));
-      const tmpPath = this.path + ".tmp";
-      await fs.writeFile(tmpPath, uniq.join("\n"), "utf8");
-      await fs.rename(tmpPath, this.path);
-    });
-  public has = (text: string): boolean => this.cache.includes(text);
-  public async add(text: string): Promise<void> {
-    if (!this.has(text)) this.__cache.push(text);
+  public get(index: number): Cache | undefined {
+    return this.cache.at(index);
+  }
+  public async add(item: Cache, unique: boolean = true): Promise<void> {
+    if ((unique && !this.has(item)) || !unique) this.__cache.push(item);
     return this.save();
   }
   public async delete(text: string): Promise<void> {
     this.__cache = this.__cache.filter((cachedId) => cachedId !== text);
     return this.save();
   }
+  public distinct(): Cache[] {
+    return Array.from(new Set(this.cache));
+  }
 }
 
-export class RecordFileManager<V> extends IOCacheManager<Record<string, V>> {
+export class RecordFileManager<Cache> extends JsonCacheManager<Record<string, Cache>> {
   /**
    * Creates an instance of RecordManager by loading from file asynchronously.
    * @param path The file path to store the record.
    */
-  public static async init<V>(path: PathLike): Promise<RecordFileManager<V>> {
-    const instance = new RecordFileManager<V>(path);
+  public static async init<Cache>(path: PathLike): Promise<RecordFileManager<Cache>> {
+    const instance = new RecordFileManager<Cache>(path);
     await instance.load();
     return instance;
   }
@@ -116,8 +129,8 @@ export class RecordFileManager<V> extends IOCacheManager<Record<string, V>> {
    * Creates an instance of RecordManager by loading from file synchronously.
    * @param path The file path to store the record.
    */
-  public static initSync<V>(path: PathLike): RecordFileManager<V> {
-    const instance = new RecordFileManager<V>(path);
+  public static initSync<Cache>(path: PathLike): RecordFileManager<Cache> {
+    const instance = new RecordFileManager<Cache>(path);
     instance.loadSync();
     return instance;
   }
@@ -127,42 +140,22 @@ export class RecordFileManager<V> extends IOCacheManager<Record<string, V>> {
    * @param path The file path to store the record.
    * @param init The initial record object.
    */
-  public constructor(path: PathLike, init: Record<string, V> = {}) {
+  public constructor(path: PathLike, init: Record<string, Cache> = {}) {
     super(path, init);
   }
 
-  public load = async (): Promise<Record<string, V>> =>
-    this.queue.set(async () => {
-      try {
-        const raw = await fs.readFile(this.path, "utf-8");
-        return (this.__cache = JSON.parse(raw));
-      } catch (e: unknown) {
-        if (isFSError(e) && e.code === "ENOENT") return {};
-        throw e;
-      }
-    });
-  public loadSync(): Record<string, V> {
-    try {
-      const raw = readFileSync(this.path, "utf-8");
-      return (this.__cache = JSON.parse(raw));
-    } catch (e: unknown) {
-      if (isFSError(e) && e.code === "ENOENT") return {};
-      throw e;
-    }
+  public has(key: string): boolean {
+    return this.cache.hasOwnProperty(key);
   }
-  public save = async (): Promise<void> =>
-    this.queue.set(async () => {
-      const tmpPath = this.path + ".tmp";
-      await fs.writeFile(tmpPath, JSON.stringify(this.cache, null, 2));
-      await fs.rename(tmpPath, this.path);
-    });
-  public hasProperty = (key: string): boolean => this.cache.hasOwnProperty(key);
-  public async set(key: string, value: V): Promise<void> {
+  public get(key: string): Cache | undefined {
+    return this.cache[key];
+  }
+  public async set(key: string, value: Cache): Promise<void> {
     this.__cache[key] = value;
     return this.save();
   }
-  public async add(key: string, value: V): Promise<void> {
-    if (!this.hasProperty(key)) this.set(key, value);
+  public async add(key: string, value: Cache): Promise<void> {
+    if (!this.has(key)) return this.set(key, value);
   }
   public async delete(key: string): Promise<void> {
     delete this.__cache[key];
@@ -170,56 +163,61 @@ export class RecordFileManager<V> extends IOCacheManager<Record<string, V>> {
   }
 }
 
-export class MapFileManager<V> extends IOCacheManager<Map<string, V>> {
-  public static async init<V>(path: PathLike): Promise<MapFileManager<V>> {
-    const instance = new MapFileManager<V>(path);
+export class MapFileManager<Cache> extends JsonCacheManager<Map<string, Cache>> {
+  public static async init<Cache>(path: PathLike): Promise<MapFileManager<Cache>> {
+    const instance = new MapFileManager<Cache>(path);
     await instance.load();
     return instance;
   }
-  public static initSync<V>(path: PathLike): MapFileManager<V> {
-    const instance = new MapFileManager<V>(path);
+  public static initSync<Cache>(path: PathLike): MapFileManager<Cache> {
+    const instance = new MapFileManager<Cache>(path);
     instance.loadSync();
     return instance;
   }
-  public constructor(path: PathLike, init: Map<string, V> = new Map()) {
+  public constructor(path: PathLike, init: Map<string, Cache> = new Map()) {
     super(path, init);
   }
 
-  public load = async (): Promise<Map<string, V>> =>
+  public load = async (): Promise<ReadonlyMap<string, Cache>> =>
     this.queue.set(async () => {
       try {
         const raw = await fs.readFile(this.path, "utf-8");
-        const obj: Record<string, V> = JSON.parse(raw);
+        const obj: Record<string, Cache> = JSON.parse(raw);
         return (this.__cache = new Map(Object.entries(obj)));
       } catch (e: unknown) {
-        if (isFSError(e) && e.code === "ENOENT") return new Map();
+        if (isFSError(e) && e.code === "ENOENT") return this.cache;
         throw e;
       }
     });
-  public loadSync(): Map<string, V> {
+  public loadSync(): ReadonlyMap<string, Cache> {
     try {
       const raw = readFileSync(this.path, "utf-8");
-      const obj: Record<string, V> = JSON.parse(raw);
+      const obj: Record<string, Cache> = JSON.parse(raw);
       return (this.__cache = new Map(Object.entries(obj)));
     } catch (e: unknown) {
-      if (isFSError(e) && e.code === "ENOENT") return new Map();
+      if (isFSError(e) && e.code === "ENOENT") return this.cache;
       throw e;
     }
   }
   public save = async (): Promise<void> =>
     this.queue.set(async () => {
-      const tmpPath = `${this.path.toString()}.tmp`;
+      const tmpPath = this.path + ".tmp";
       const obj = Object.fromEntries(this.cache);
       await fs.writeFile(tmpPath, JSON.stringify(obj, null, 2));
       await fs.rename(tmpPath, this.path);
     });
-  public hasProperty = (key: string): boolean => this.cache.has(key);
-  public async set(key: string, value: V): Promise<void> {
+  public has(key: string): boolean {
+    return this.cache.has(key);
+  }
+  public get(key: string): Cache | undefined {
+    return this.cache.get(key);
+  }
+  public async set(key: string, value: Cache): Promise<void> {
     this.__cache.set(key, value);
     return this.save();
   }
-  public async add(key: string, value: V): Promise<void> {
-    if (!this.hasProperty(key)) this.set(key, value);
+  public async add(key: string, value: Cache): Promise<void> {
+    if (!this.has(key)) this.set(key, value);
   }
   public async delete(key: string): Promise<void> {
     this.__cache.delete(key);
