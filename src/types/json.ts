c734e00f7c052a55fb8ab2883ed9bace32acde0b
もちこ♥ <mochitarouji@gmail.com>
2026-01-17 01:31:22 +0900

    Added type JSONParsed

--- src/types/json.ts
+++ src/types/json.ts
@@ -2,6 +2,16 @@ export type JSONParsable =
   | string
   | number
   | boolean
-  | { [key: string]: JSONParsable }
+  | {
+      [K in keyof any]: JSONParsable;
+    }
   | Array<JSONParsable>
   | null;
+
+export type JSONParsed<T> = T extends string | number | boolean | null
+  ? T
+  : T extends Array<infer U>
+  ? JSONParsed<U>[]
+  : T extends object
+  ? { [K in keyof T]: JSONParsed<T[K]> }
+  : never;
